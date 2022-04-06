d5c4da5a91d04e725d5be79135aa85d6ae0c324b
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 11:37:55 -0700

    Add basic About and Login components...no routing

--- src/index.js
+++ src/index.js
@@ -10,6 +10,31 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  )
+}
+
 function App() {
   return (
     <Route path="/">
