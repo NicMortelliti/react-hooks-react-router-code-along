ea81175f6eee0909b02213bb7e842d60de07afad
NicMortelliti <npmortelliti@gmail.com>
2022-04-06 12:38:35 -0700

    Fill out individual app components

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+// src/components/App.js
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Login from "./Login";
+import NavBar from "./NavBar";
+import Signup from "./Signup";
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Switch>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+      <Switch>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
